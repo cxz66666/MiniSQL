d11ac71efea0daca5f2563a77e85eaf185a74fd8
Hans <2655098959@qq.com>
2021-06-13 19:36:04 +0800

    Finish the shit of query

--- src/IndexManager/indexmanager.go
+++ src/IndexManager/indexmanager.go
@@ -141,19 +141,94 @@ func Delete(info IndexInfo, key_value value.Value, pos Position) error {
 	return nil
 }
 
-func GetFirst(info IndexInfo, key_value value.Value, compare_type value.CompareType) (Position, error) {
-	return *new(Position), nil
+type ResultNode struct {
+	Pos       Position
+	next_node *ResultNode
 }
 
-// 返回刚才查询的结果中的下一个元素
-// 如果访问越界，第二个返回值为 false
-func GetNext() (Position, bool, error) {
-	return *new(Position), true, nil
+func (node *ResultNode) GetNext() *ResultNode {
+	return node.next_node
 }
 
-// 查询结束后，调用此函数
-func Free() {
+func GetFirst(info IndexInfo, key_value value.Value, compare_type value.CompareType) (*ResultNode, error) {
+	filename := info.getFileName()
+	key_length := info.Attr_length
 
+	cur_node, cur_node_block := getBpNode(filename, 0, key_length)
+	var i uint16
+	if compare_type == value.Equal || compare_type == value.GreatEqual || compare_type == value.Great {
+		// Find the first node that is great or equal to the node
+		for {
+			n := cur_node.getSize()
+			var cur_compare_type value.CompareType
+			var cur_n uint16
+			if cur_node.isLeaf() == 1 {
+				cur_compare_type = value.GreatEqual
+				cur_n = n
+			} else {
+				cur_compare_type = compare_type
+				cur_n = n - 1
+			}
+			for i = 0; i <= cur_n; i++ {
+				if res, _ := key_value.Compare(cur_node.getKey(info.Attr_type, i), cur_compare_type); res {
+					break
+				}
+			}
+			if cur_node.isLeaf() == 1 {
+				break
+			}
+			next_node_id := cur_node.getPointer(i)
+			next_node, next_node_block := getBpNode(filename, next_node_id, key_length)
+			cur_node_block.FinishRead()
+			cur_node = next_node
+			cur_node_block = next_node_block
+		}
+	} else {
+		// Get the first node of all nodes
+		for cur_node.isLeaf() == 0 {
+			next_node_id := cur_node.getPointer(0)
+			next_node, next_node_block := getBpNode(filename, next_node_id, key_length)
+			cur_node_block.FinishRead()
+			cur_node = next_node
+			cur_node_block = next_node_block
+		}
+		i = 0
+	}
+
+	dummy_head := new(ResultNode)
+	cur_result_node := dummy_head
+
+	for {
+		failed := false
+		n := cur_node.getSize()
+		if i > n { // Switch to the next node
+			next_node_id := cur_node.getNext()
+			next_node, next_node_block := getBpNode(filename, next_node_id, key_length)
+			cur_node_block.FinishRead()
+			cur_node = next_node
+			cur_node_block = next_node_block
+			i = 0
+		}
+
+		for j := i; j <= n; j++ {
+			if res, _ := key_value.Compare(cur_node.getKey(info.Attr_type, j), compare_type); !res {
+				failed = true
+				break
+			}
+			new_result_node := new(ResultNode)
+			*new_result_node = ResultNode{
+				Pos:       cur_node.getFilePointer(j),
+				next_node: nil,
+			}
+			cur_result_node.next_node = new_result_node
+			cur_result_node = new_result_node
+		}
+		if failed {
+			break
+		}
+	}
+
+	return dummy_head.next_node, nil
 }
 
 // pos_in_record 索引字段在 record 中的 offset，单位为 byte
